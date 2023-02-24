280fe89e990671f51f40ed6f45e23eef926e431e
wangwei1 <578895238@qq.com>
2023-02-24 17:31:55 +0800

    demo

--- src/pages/components/list/index.jsx
+++ src/pages/components/list/index.jsx
@@ -1,57 +1,63 @@
-import React, { Component, createRef } from 'react'
-import { View } from '@tarojs/components'
-import VirtualList from '@tarojs/components/virtual-list'
+import React, { Component, createRef } from "react";
+import { View } from "@tarojs/components";
+import VirtualList from "@tarojs/components/virtual-list";
 
 function buildData(offset = 0) {
   return Array(100)
     .fill(0)
-    .map((_, i) => i + offset)
+    .map((_, i) => i + offset);
 }
 
 const Row = React.memo(({ id, index, data }) => {
   return (
-    <View id={id} style={{ borderColor: 'black', borderWidth: 1, borderStyle: 'solid' }}>
-      <View>
-        <View>
-          <View>
-            <View>
-              <View>
-                Row {index} : {data[index]}
-              </View>
-            </View>
-          </View>
-        </View>
-      </View>
+    <View id={id} style={{ border: "1px solid black", padding: 10 }}>
+      Row {index} : {data[index]}
     </View>
-  )
-})
+  );
+});
 
 export default class List extends Component {
   state = {
     data: buildData(0),
-  }
+  };
 
-  list = createRef()
+  list = createRef();
 
   render() {
-    const { data } = this.state
-    const dataLen = data.length
+    const { data } = this.state;
+    const dataLen = data.length;
     return (
-      <>
-        <View onClick={() => {
-          this.list.current?.scrollToItem(50, 'start')
-        }}
-        >111111111</View>
+      <View style={{ width: "100vw", height: "100vh" }}>
+        <View
+          style={{
+            position: "fixed",
+            zIndex: 999,
+            borderRadius: "50%",
+            width: 30,
+            height: 30,
+            right: 20,
+            bottom: 20,
+            padding: 14,
+            borderWidth: 2,
+            borderColor: "black",
+            borderStyle: "dashed",
+          }}
+          onClick={() => {
+            this.list.current?.scrollToItem(50, "start");
+          }}
+        >
+          Click
+        </View>
         <VirtualList
           ref={this.list}
           height={800} /* 列表的高度 */
-          width='100%' /* 列表的宽度 */
+          width="100vw" /* 列表的宽度 */
           item={Row} /* 列表单项组件，这里只能传入一个组件 */
           itemData={data} /* 渲染列表的数据 */
           itemCount={dataLen} /* 渲染列表的长度 */
-          itemSize={100} /* 列表单项的高度  */
+          itemSize={50} /* 列表单项的高度  */
         />
-      </>
-    )
+      </View>
+    );
   }
 }
